f1e3c9f3e45eaafd5f176cda14db6c0052237482
MISMOGPE\VABE <valentin.bertho@mismo.fr>
2025-11-18 11:56:18 +0100

    MAJ URL API

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import { useAuthStore } from '../store/authStore';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5141/api'
+  //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5141/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://trainmate-9079.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
